18e2084c564bebac7269840472f39d4bfc87a439
Jakub <jakub.soad@gmail.com>
2019-05-06 13:44:08 +0200

    init

---
+++ Newsletter/index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Newsletter</title>
+</head>
+<body>
+<h1>Newsletter</h1>
+
+<form method="post" action="save.php">
+    <input type="text" name="email" placeholder="E-mail...">
+    <br><br>
+
+    <?php
+    if (isset($_SESSION['wrongEmail']))
+        echo $_SESSION['wrongEmail'];
+
+    ?>
+    <br><br>
+    <input type="submit" value="Submit!">
+
+
+</form>
+
+</body>
+
+</html>
---
+++ Newsletter/save.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+require_once 'database.php';
+
+if (isset($_POST['email']))
+{
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+    if (empty($email))
+    {
+        header("Location: index.php");
+        $_SESSION['wrongEmail'] = "Wrong E-mail adress";
+    }
+    else
+    {
+        $query = $db->prepare('INSERT INTO users VALUES (NULL, :email)');
+        $query->bindValue(':email', $email, PDO::PARAM_STR);
+        $query->execute();
+    }
+}
+else
+{
+    header("Location: index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Thank you!</title>
+</head>
+<body>
+<h1>Thank you <?php echo $email?>!</h1>
+
+
+
+
+</body>
+
+</html>
